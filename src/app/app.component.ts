02b345fd5639dec5e55acc49c7700ba1b70f0b60
Michelle <mpuschkarow@web.de>
2025-06-19 10:59:46 +0200

    contact form styling, gloabl mixins for fonts defined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component'; // Diese Zeile hinzufügen
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent], // SidebarComponent hier hinzufügen
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, ContactFormComponent], // SidebarComponent hier hinzufügen
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
