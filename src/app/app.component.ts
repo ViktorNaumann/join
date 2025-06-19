b8f87858b1fe3256c5368e234665a74ce421b217
Marco <marco.palummieri@gmail.com>
2025-06-19 23:31:15 +0200

    add contact details component with styling and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component'; // Diese Zeile hinzufügen
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, ContactFormComponent], // SidebarComponent hier hinzufügen
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, ContactFormComponent, ContactDetailsComponent], // SidebarComponent hier hinzufügen
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
