bfe921ce5b7dfe3172bed6e6ec720d19055c9fee
Marco <marco.palummieri@gmail.com>
2025-06-30 16:59:12 +0200

    feat: implement drag-and-drop functionality for task management in board component

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
 export class BoardComponent {
+  // Test-Daten für die Drag & Drop Funktionalität
+  todo:string[] = ['Task 1', 'Task 2', 'Task 3'];
+  inprogress: string[] = ['Completed Task 1'];
+  awaitfeedback: string[] = [];
+  done: string[] = [];
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
