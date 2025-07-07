572dd42a46153d10868bafef4f06883f3362dc2e
Marco <marco.palummieri@gmail.com>
2025-07-07 10:54:26 +0200

    style: enhance responsive design for header, task columns, and sidebar; adjust layout for better mobile experience

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,22 +5,26 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   animations: [
     trigger('slideInOut', [
       transition(':enter', [
         style({ transform: 'translateX(100%)', opacity: 0 }),
-        animate('200ms ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
+        animate(
+          '200ms ease-out',
+          style({ transform: 'translateX(0)', opacity: 1 })
+        ),
       ]),
       transition(':leave', [
-        animate('200ms ease-in', style({ transform: 'translateX(100%)', opacity: 0 }))
-      ])
-    ])
-  ]
+        animate(
+          '200ms ease-in',
+          style({ transform: 'translateX(100%)', opacity: 0 })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HeaderComponent {
   menuOpen = false;
@@ -36,8 +40,6 @@ export class HeaderComponent {
   }
 
   toggleMenu() {
-    if (this.isMobile) {
-      this.menuOpen = !this.menuOpen;
-    }
+    this.menuOpen = !this.menuOpen;
   }
 }
