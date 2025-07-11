9fa0984f2d4de119b92e3987323472d500950c0c
Michelle <mpuschkarow@web.de>
2025-07-11 14:24:40 +0200

    router Module imported, font color added

--- src/app/shared/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/privacy-policy/privacy-policy.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
