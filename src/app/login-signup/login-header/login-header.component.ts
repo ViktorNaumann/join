66227650dd434aa75c82121ba5f197c97f2452a5
afk-Jane <j.domsgen@protonmail.com>
2025-07-11 15:26:56 +0200

    hyperlinks added and styled

--- src/app/login-signup/login-header/login-header.component.ts
+++ src/app/login-signup/login-header/login-header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-header',
-  imports: [],
+  imports: [
+    RouterModule,
+  ],
   templateUrl: './login-header.component.html',
   styleUrl: './login-header.component.scss'
 })
--- src/app/login-signup/signup/signup.component.ts
+++ src/app/login-signup/signup/signup.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   imports: [
+    RouterModule,
     FooterComponent
   ],
   templateUrl: './signup.component.html',
