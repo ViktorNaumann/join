443ca8cb7117d9a7e01b881a3efd4426de4b8a5c
Michelle <mpuschkarow@web.de>
2025-07-11 14:16:42 +0200

    styling for help and legal notice page

--- src/app/shared/help/help.component.ts
+++ src/app/shared/help/help.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-help',
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './help.component.html',
   styleUrl: './help.component.scss'
 })
