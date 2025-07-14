c643cd403fb8ce898adfb40e87d176ef8060db98
Michelle <mpuschkarow@web.de>
2025-07-14 18:32:24 +0200

    logo transition switched  to css keyframes

--- src/app/login-signup/login/login.component.ts
+++ src/app/login-signup/login/login.component.ts
@@ -21,30 +21,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   animations: [
-    trigger('moveLogo', [
-      state('start', style({
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        opacity: 1
-      })),
-      state('moved', style({
-        top: '0',
-        left: '0',
-        transform: 'translate(0, 0)',
-        opacity: 0
-      })),
-      transition('start => moved', [
-        group([
-          animate('2.5s 0.5s ease-in-out', style({
-            top: '0',
-            left: '0',
-            transform: 'translate(0, 0)'
-          })),
-          animate('2s 0.5s ease-in-out', style({ opacity: 0 }))
-        ])
-      ])
-    ]),
     trigger('fadeOutWrapper', [
       state('start', style({
         opacity: 1
@@ -53,17 +29,19 @@ import { CommonModule } from '@angular/common';
         opacity: 0
       })),
       transition('start => moved', [
-        animate('1.5s 0.25s ease-in-out')
+        animate('1.5s 0.5s ease-in-out')
       ])
     ])
   ]
 })
 export class LoginComponent {
   logoState: 'start' | 'moved' = 'start';
+  pageLoaded = false;
 
   ngOnInit(): void {
     if (!sessionStorage.getItem('logoMoved')) {
       setTimeout(() => {
+        this.pageLoaded = true;
         this.logoState = 'moved';
         sessionStorage.setItem('logoMoved', 'true');
       }, 100);
