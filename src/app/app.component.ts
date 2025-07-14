293e758308acfcb7b014cacf6f403f4097ee478a
Michelle <mpuschkarow@web.de>
2025-07-14 09:59:03 +0200

    url history service used to redirect to last url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
+import { NavigationHistoryService } from './services/navigation-history.service';
 
 @Component({
   selector: 'app-root',
@@ -17,5 +18,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  constructor(private navigationService: NavigationHistoryService) {}
   title = 'join';
+
 }
