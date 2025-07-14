7e1a07b6e28fb283ad45692b6cfcb9c13aa53197
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-07-14 14:34:25 +0200

    Add conditional display for header and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
 import { NavigationHistoryService } from './services/navigation-history.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +19,17 @@ import { NavigationHistoryService } from './services/navigation-history.service'
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor(private navigationService: NavigationHistoryService) {}
-  title = 'join';
+  showHeaderAndSidebar = true;
 
+  constructor(private navigationService: NavigationHistoryService, private router: Router) {
+    // Überwache Route-Änderungen
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // Verstecke Header und Sidebar für Login- und Signup-Seiten
+      this.showHeaderAndSidebar = !['/login', '/', '/signup'].includes(event.urlAfterRedirects);
+    });
+  }
+  
+  title = 'join';
 }
